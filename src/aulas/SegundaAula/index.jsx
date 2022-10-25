44042d1b3b0a3acdf9c1860c1e262201916d35ef
Miguel Oduber <miguel.oduber@gmail.com>
2022-10-24 21:19:23 -0300

    Primera Aula - Configurando vite, npm e react

---
+++ src/aulas/SegundaAula/index.jsx
@@ -0,0 +1,28 @@
+import './style.scss'
+
+export function SegundaAula(){
+    return (
+        <main>
+            <h1>Segunda Aula</h1>
+            <h2>Projeto escolhido: Netflix</h2>
+            <ul>
+                <li>Primeiro componente:</li>
+                <p> - Navibar</p>
+                <p> --- Logo</p>
+                <p> --- Menu</p>
+                <p> --- Procura</p>
+                <p> --- Classificação</p>
+                <p> --- Notificações</p>
+                <p> --- Usuário ativo</p>
+                <li>Segundo componente:</li>
+                <p> - Banner</p>
+                <p> --- Imagem de fundo</p>
+                <p> --- Título do filme</p>
+                <p> --- Descrição do filme</p>
+                <p> --- Botão assistir</p>
+                <p> --- Botão Mais informações</p>
+
+            </ul>
+        
+        </main>)
+}
